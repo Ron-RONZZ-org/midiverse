4a590fcc0feec2fd487d003d46fa8740c64ab8b6
Rong Zhou <ron@ronzz.org>
2025-12-05 15:41:58 +0000

    ec osystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'midiverse-backend',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       cwd: '/var/www/midiverse-deployment/midiverse',
       env: {
         NODE_ENV: 'production',
         PORT: 3010,
         FRONTEND_URL: "https://midiverse.org",
       },
-      instances: 2,
-      exec_mode: 'cluster',
+      instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
